726edf5487fc313b58829632a4e998cf530fbc14
NoRaincheck <2498638+NoRaincheck@users.noreply.github.com>
2026-06-04 00:56:34 +1000

    fix failing tests

--- healthapp/index.ts
+++ healthapp/index.ts
@@ -10,7 +10,7 @@ app.get("/ping/", (_req, res) => {
 });
 
 app.get("/hello/", (_req, res) => {
-  res.send("Hello Full Stack");
+  res.send("Hello Full Stack!");
 });
 
 app.get("/bmi", (req, res) => {
