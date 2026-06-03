d268916e3acd16f514a84d1dda0e5549d32dcd48
NoRaincheck <2498638+NoRaincheck@users.noreply.github.com>
2026-06-04 01:47:33 +1000

    complete exercise 11

--- patientor/backend/index.ts
+++ patientor/backend/index.ts
@@ -1,13 +1,18 @@
 import express from 'express';
+import data from './data/diagnoses.ts';
 const app = express();
 app.use(express.json());
 
-const PORT = 3000;
+const PORT = 3001;
 
 app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
+
+app.get('/api/diagnoses', (_req, res) => {
+  res.send(data);
+})
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
